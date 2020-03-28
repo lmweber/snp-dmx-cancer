748c666d2f4ed71b0efa357e842d34d4f6bc4b88
Lukas Weber <lukas.weber.edu@gmail.com>
2020-03-28 00:44:48 -0400

    use gzipped FASTQ files; fix output directories

--- run_STARsolo_16030X3.sh
+++ run_STARsolo_16030X3.sh
@@ -13,12 +13,13 @@
 #qsub -V -cwd -pe local 20 -l mem_free=10G,h_vmem=11G run_STARsolo.sh
 
 # note: R2 is before R1 in --readFilesIn
-# note: requires unzipped FASTQ files on JHPCE cluster due to file system
 
 
 # STAR --genomeDir ../data/STAR_index \
+# --readFilesCommand zcat \
+# --outFileNamePrefix ./16030X2/ \
 # --runThreadN 20 \
-# --readFilesIn ../data/16030R/Fastq/16030X2_HJVMLDMXX/16030X2_HJVMLDMXX_S1_L002_R2_001.fastq ../data/16030R/Fastq/16030X2_HJVMLDMXX/16030X2_HJVMLDMXX_S1_L002_R1_001.fastq \
+# --readFilesIn ../data/16030R/Fastq/16030X2_HJVMLDMXX/16030X2_HJVMLDMXX_S1_L002_R2_001.fastq.gz ../data/16030R/Fastq/16030X2_HJVMLDMXX/16030X2_HJVMLDMXX_S1_L002_R1_001.fastq.gz \
 # --soloType CB_UMI_Simple \
 # --soloCBwhitelist ../data/cellranger_whitelist/3M-february-2018.txt \
 # --soloUMIlen 12 \
@@ -26,8 +27,10 @@
 # --soloCBmatchWLtype 1MM_multi_pseudocounts
 
 STAR --genomeDir ../data/STAR_index \
+--readFilesCommand zcat \
+--outFileNamePrefix ./16030X3/ \
 --runThreadN 20 \
---readFilesIn ../data/16030R/Fastq/16030X3_HJTWLDMXX/16030X3_HJTWLDMXX_S1_L001_R2_001.fastq ../data/16030R/Fastq/16030X3_HJTWLDMXX/16030X3_HJTWLDMXX_S1_L001_R1_001.fastq  \
+--readFilesIn ../data/16030R/Fastq/16030X3_HJTWLDMXX/16030X3_HJTWLDMXX_S1_L001_R2_001.fastq.gz ../data/16030R/Fastq/16030X3_HJTWLDMXX/16030X3_HJTWLDMXX_S1_L001_R1_001.fastq.gz  \
 --soloType CB_UMI_Simple \
 --soloCBwhitelist ../data/cellranger_whitelist/3M-february-2018.txt \
 --soloUMIlen 12 \
@@ -35,8 +38,10 @@ STAR --genomeDir ../data/STAR_index \
 --soloCBmatchWLtype 1MM_multi_pseudocounts
 
 # STAR --genomeDir ../data/STAR_index \
+# --readFilesCommand zcat \
+# --outFileNamePrefix ./16030X4/ \
 # --runThreadN 20 \
-# --readFilesIn ../data/16030R/Fastq/16030X4_HJTWLDMXX/16030X4_HJTWLDMXX_S2_L001_R2_001.fastq ../data/16030R/Fastq/16030X4_HJTWLDMXX/16030X4_HJTWLDMXX_S2_L001_R1_001.fastq  \
+# --readFilesIn ../data/16030R/Fastq/16030X4_HJTWLDMXX/16030X4_HJTWLDMXX_S2_L001_R2_001.fastq.gz ../data/16030R/Fastq/16030X4_HJTWLDMXX/16030X4_HJTWLDMXX_S2_L001_R1_001.fastq.gz  \
 # --soloType CB_UMI_Simple \
 # --soloCBwhitelist ../data/cellranger_whitelist/3M-february-2018.txt \
 # --soloUMIlen 12 \
