252905d2b1ce1ac80fe1e1e8c23d9d3bd34d5d2e
Lukas Weber <lukas.weber.edu@gmail.com>
2020-03-27 23:08:35 -0400

    script to create STAR genome index

---
+++ run_STAR_index.sh
@@ -0,0 +1,23 @@
+# Bash script to run STAR to create genome index for STARsolo
+
+# STARsolo is a faster and less buggy alternative to Cell Ranger
+# see https://github.com/alexdobin/STAR/blob/master/docs/STARsolo.md
+
+
+# --------------
+# generate index
+# --------------
+
+# generate STAR genome index using Cell Ranger annotation files (only needs to be run once)
+
+#qsub -V -cwd -pe local 20 -l mem_free=10G,h_vmem=11G run_STAR_index.sh
+
+
+STAR --runMode genomeGenerate \
+--runThreadN 20 \
+--genomeDir ../data/STAR_index \
+--genomeFastaFiles ../data/GRCh38/refdata-cellranger-GRCh38-3.0.0/fasta/genome.fa \
+--sjdbGTFfile ../data/GRCh38/refdata-cellranger-GRCh38-3.0.0/genes/genes.gtf \
+--genomeSAsparseD 3
+
+
