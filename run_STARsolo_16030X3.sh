00213bfd7d1b551062382394300313b7c04a56dd
Lukas Weber <lukas.weber.edu@gmail.com>
2020-03-28 16:41:19 -0400

    fix STARsolo BAM output

--- run_STARsolo_16030X3.sh
+++ run_STARsolo_16030X3.sh
@@ -15,9 +15,12 @@
 # note: R2 is before R1 in --readFilesIn
 
 
+# mkdir 16030X2
+# 
 # STAR --genomeDir ../data/STAR_index \
 # --readFilesCommand zcat \
 # --outFileNamePrefix ./16030X2/ \
+# --outSAMtype BAM Unsorted \
 # --runThreadN 20 \
 # --readFilesIn ../data/16030R/Fastq/16030X2_HJVMLDMXX/16030X2_HJVMLDMXX_S1_L002_R2_001.fastq.gz ../data/16030R/Fastq/16030X2_HJVMLDMXX/16030X2_HJVMLDMXX_S1_L002_R1_001.fastq.gz \
 # --soloType CB_UMI_Simple \
@@ -26,9 +29,13 @@
 # --soloUMIfiltering MultiGeneUMI \
 # --soloCBmatchWLtype 1MM_multi_pseudocounts
 
+
+mkdir 16030X3
+
 STAR --genomeDir ../data/STAR_index \
 --readFilesCommand zcat \
 --outFileNamePrefix ./16030X3/ \
+--outSAMtype BAM Unsorted \
 --runThreadN 20 \
 --readFilesIn ../data/16030R/Fastq/16030X3_HJTWLDMXX/16030X3_HJTWLDMXX_S1_L001_R2_001.fastq.gz ../data/16030R/Fastq/16030X3_HJTWLDMXX/16030X3_HJTWLDMXX_S1_L001_R1_001.fastq.gz  \
 --soloType CB_UMI_Simple \
@@ -37,9 +44,13 @@ STAR --genomeDir ../data/STAR_index \
 --soloUMIfiltering MultiGeneUMI \
 --soloCBmatchWLtype 1MM_multi_pseudocounts
 
+
+# mkdir 16030X4
+# 
 # STAR --genomeDir ../data/STAR_index \
 # --readFilesCommand zcat \
 # --outFileNamePrefix ./16030X4/ \
+# --outSAMtype BAM Unsorted \
 # --runThreadN 20 \
 # --readFilesIn ../data/16030R/Fastq/16030X4_HJTWLDMXX/16030X4_HJTWLDMXX_S2_L001_R2_001.fastq.gz ../data/16030R/Fastq/16030X4_HJTWLDMXX/16030X4_HJTWLDMXX_S2_L001_R1_001.fastq.gz  \
 # --soloType CB_UMI_Simple \
