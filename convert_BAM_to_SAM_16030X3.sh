2e542e3a778578a919180c3213bcb8fd79e6cdf0
Lukas Weber <lukas.weber.edu@gmail.com>
2020-05-04 08:51:56 -0400

    rename files and add qsub commands

--- convert_BAM_to_SAM_16030X3.sh
+++ convert_BAM_to_SAM_16030X3.sh
@@ -3,6 +3,9 @@
 # required to parse SAM output file
 
 
+#qsub -V -cwd -l mem_free=100G,h_vmem=200G,h_fsize=200G convert_BAM_to_SAM.sh
+
+
 samtools view -h 16030X3_HJTWLDMXX/outs/possorted_genome_bam.bam > 16030X3_HJTWLDMXX/outs/possorted_genome_bam.sam
 
 
