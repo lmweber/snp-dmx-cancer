9233836cbf77b0331980b72b2151f47651a77573
Lukas Weber <lukas.weber.edu@gmail.com>
2020-05-05 13:02:45 -0400

    convert parsed SAM back to BAM

---
+++ convert_parsed_SAM_to_BAM_16030X3.sh
@@ -0,0 +1,11 @@
+# Convert parsed SAM file back to BAM format
+
+# required otherwise merged SAM file is too large
+
+
+#qsub -V -cwd -l mem_free=100G,h_vmem=300G,h_fsize=300G convert_parsed_SAM_to_BAM.sh
+
+
+samtools view -S -b 16030X3_HJTWLDMXX/outs/possorted_genome_bam.sam > 16030X3_HJTWLDMXX/outs/possorted_genome_bam_parsed.bam
+
+
