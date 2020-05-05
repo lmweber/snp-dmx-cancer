348a31a5c90e7b4077b4cee603d57644321070df
Lukas Weber <lukas.weber.edu@gmail.com>
2020-05-05 08:04:25 -0400

    max file sizes

--- convert_BAM_to_SAM_16030X4.sh
+++ convert_BAM_to_SAM_16030X4.sh
@@ -3,7 +3,7 @@
 # required to parse SAM output file
 
 
-#qsub -V -cwd -l mem_free=100G,h_vmem=200G,h_fsize=300G convert_BAM_to_SAM.sh
+#qsub -V -cwd -l mem_free=100G,h_vmem=300G,h_fsize=300G convert_BAM_to_SAM.sh
 
 
 samtools view -h 16030X4_HJTWLDMXX/outs/possorted_genome_bam.bam > 16030X4_HJTWLDMXX/outs/possorted_genome_bam.sam
