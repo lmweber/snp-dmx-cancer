254b0abda3f779a726bed37cf7b7370fa3061940
Lukas Weber <lukas.weber.edu@gmail.com>
2020-08-02 16:49:40 -0400

    updated script to download EGA files - med/high TMB only

--- download_EGA/download_EGA_subset_EGAD00001005054.sh
+++ download_EGA/download_EGA_subset_EGAD00001005054.sh
@@ -1,89 +1,90 @@
+# -------------------------------------------------------
+# Script to download lung adenocarcinoma dataset from EGA
+# -------------------------------------------------------
+
 # downloading only intermediate and high TMB samples
 
-# sample T06 (note last filename is out of order in original file list) (low TMB)
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482037
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482038
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482039
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482040
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482042
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482041
 
 # sample T08 (int TMB)
+mkdir LUNG_T08
+cd LUNG_T08
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482043
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482044
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482045
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482046
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482047
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482048
+cd ..
+# flatten directories
+find LUNG_T08 -mindepth 2 -type f -exec mv -t LUNG_T08 -i '{}' +
+
 
 # sample T09 (high TMB)
+mkdir LUNG_T09
+cd LUNG_T09
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482049
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482050
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482051
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482052
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482053
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482054
+cd ..
+# flatten directories
+find LUNG_T09 -mindepth 2 -type f -exec mv -t LUNG_T09 -i '{}' +
 
-# sample T18 (low TMB)
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482055
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482056
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482057
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482058
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482059
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482060
-
-# sample T19 (low TMB)
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482061
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482062
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482063
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482064
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482065
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482066
 
 # sample T20 (high TMB)
+mkdir LUNG_T20
+cd LUNG_T20
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482067
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482068
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482069
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482070
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482071
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482072
+cd ..
+# flatten directories
+find LUNG_T20 -mindepth 2 -type f -exec mv -t LUNG_T20 -i '{}' +
+
 
 # sample T25 (int TMB)
+mkdir LUNG_T25
+cd LUNG_T25
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482073
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482074
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482075
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482076
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482077
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482078
+cd ..
+# flatten directories
+find LUNG_T25 -mindepth 2 -type f -exec mv -t LUNG_T25 -i '{}' +
+
 
 # sample T28 (high TMB)
+mkdir LUNG_T28
+cd LUNG_T28
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482079
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482080
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482081
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482082
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482083
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482084
+cd ..
+# flatten directories
+find LUNG_T28 -mindepth 2 -type f -exec mv -t LUNG_T28 -i '{}' +
 
-# sample T30 (low TMB)
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482085
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482086
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482087
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482088
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482089
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482090
 
 # sample T31 (int TMB)
+mkdir LUNG_T31
+cd LUNG_T31
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482091
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482092
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482093
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482094
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482095
 pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482096
+cd ..
+# flatten directories
+find LUNG_T31 -mindepth 2 -type f -exec mv -t LUNG_T31 -i '{}' +
 
-# sample T34 (low TMB)
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482097
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482098
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482099
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482100
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482101
-#pyega3 -cf ~/EGA/credentials.json -c 50 fetch EGAF00002482102
