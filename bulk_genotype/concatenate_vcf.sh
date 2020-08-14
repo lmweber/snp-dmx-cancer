955101c41aba4c434de37cd771284fc83487d887
Lukas Weber <lukas.weber.edu@gmail.com>
2020-08-14 09:28:17 -0400

    concatenate VCF files (bulk samples)

---
+++ bulk_genotype/concatenate_vcf.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#######################################
+# Shell script to concatenate VCF files
+#######################################
+
+# This script concatenates VCF files from all 3 samples in the HGSOC dataset, 
+# generated with the scripts "bulk_genotype_HGSOC_17667XX.sh".
+
+
+# qsub -V -cwd -pe local 20 -l mem_free=2G,h_vmem=3G,h_fsize=100G concatenate_vcf.sh
+
+
+# ---------------------
+# Concatenate VCF files
+# ---------------------
+
+# convert gzipped output files to bgzipped format (required by vcftools)
+
+gunzip -c ../../bulk/17667X1/cellSNP/cellSNP.cells.vcf.gz > ../../bulk/17667X1/cellSNP/cellSNP.cells-bgz.vcf
+bgzip ../../bulk/17667X1/cellSNP/cellSNP.cells-bgz.vcf
+gunzip -c ../../bulk/17667X2/cellSNP/cellSNP.cells.vcf.gz > ../../bulk/17667X2/cellSNP/cellSNP.cells-bgz.vcf
+bgzip ../../bulk/17667X2/cellSNP/cellSNP.cells-bgz.vcf
+gunzip -c ../../bulk/17667X3/cellSNP/cellSNP.cells.vcf.gz > ../../bulk/17667X3/cellSNP/cellSNP.cells-bgz.vcf
+bgzip ../../bulk/17667X3/cellSNP/cellSNP.cells-bgz.vcf
+
+
+# concatenate VCF files using vcftools (vcr-concat)
+
+mkdir -p ../../bulk/cellSNP_merged
+
+vcf-concat ../../bulk/17667X1/cellSNP/cellSNP.cells-bgz.vcf.gz ../../bulk/17667X2/cellSNP/cellSNP.cells-bgz.vcf.gz ../../bulk/17667X3/cellSNP/cellSNP.cells-bgz.vcf.gz | \
+gzip -c > \
+../../bulk/cellSNP_merged/cellSNP.cells-merged.vcf.gz
+
