9d9c3ee469e8e4f32341b44ee74bbac1c33b3d71
Lukas Weber <lukas.weber.edu@gmail.com>
2020-08-20 10:23:53 -0400

    update qsub command

--- genotype/genotype_bulk_bcftools/genotype_bulk_HGSOC_bcftools.sh
+++ genotype/genotype_bulk_bcftools/genotype_bulk_HGSOC_bcftools.sh
@@ -10,7 +10,7 @@
 
 # note: requires BAM files from previous scripts "align_index_bulk_HGSOC_17667XX.sh"
 
-# qsub -V -cwd -pe local 10 -l mem_free=10G,h_vmem=11G,h_fsize=100G genotype_bulk_HGSOC_bcftools.sh
+# qsub -V -cwd -l mem_free=10G,h_vmem=11G,h_fsize=100G genotype_bulk_HGSOC_bcftools.sh
 
 
 # start runtime
